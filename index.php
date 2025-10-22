ace9395c20067b34a1160ba05efedb45346fd50e
arielfelippi <41402358+arielfelippi@users.noreply.github.com>
2025-10-21 21:58:31 -0300

    exercicios corrigidos

--- index.php
+++ index.php
@@ -116,22 +116,58 @@ echo "<br>A Média é: $mediaHarmonica<br>";
 
 // Calcular o volume de uma esfera com raio de 5cm.
 // Dicas: Elevar ao cubo -> 2 ** 3 = 8. PI = 3.14
+// Formula: 4/3 * PI * R ao cubo.
+CONST PI = 3.14;
+$raioAoCubo = 5 ** 3;
+$fracao = 4/3;
+$volumeEsfera = $fracao * PI * $raioAoCubo;
+echo "<br>O volume da Esfera é: $volumeEsfera<br>";
 
 
 // Calcular a Area de um triangulo que tenha uma base de 8cm
 // e sua altura de 6cm.
+$base = 8;
+$altura = 6;
+$areaTriangulo = ($base * $altura) / 2;
+echo "<br>A area do triangulo é: $areaTriangulo<br>";
 
 
 // Calcular quantos segundos tem em 2 horas e 30 minutos.
+$horas = 2;
+$minutos = 30;
+$minutosParaSegundos = 30 * 60; // 1800
+
+$horasParaMinutos = 2 * 60; // 120
+$horasSegundos = $horasParaMinutos * 60; // 7200
+
+$totalEmSegundos = $horasSegundos + $minutosParaSegundos;  // 7200 + 1800 = 9000
+echo "<br>O Total em segundos é: $totalEmSegundos<br>";
 
 
 // Calcular IMC de uma Pessoa. Podem usar quaisquer valores.
+$peso = 70;
+$altura = 1.88;
+$imc = round( $peso / ($altura ** 2), 2 );
+echo "<br>O IMC é: $imc<br>";
 
 
 // Converter graus Celsius para Fahrenheit. 35ºC -> ?ºF
+$celsius = 35;
+$fahrenheit = ($celsius * 9/5) + 32;
+echo "<br>O A temperatura em graus Fahrenheit é: $fahrenheit ºF <br>";
 
 
 // Aplicar a formula de Bhaskara para a seguinte equação do segundo grau:
 // $a = 1; $b = -3; $c =-4;
+// sqrt -> pow -> ** = potenciaciao
+$a = 1; 
+$b = -3; 
+$c =-4;
 
+$delta = ($b ** 2) - (4 * $a * $c); // OU
+$delta = ($b * $b) - (4 * $a * $c);
+
+$x1 = (-$b + sqrt($delta)) / (2 * $a);
+$x2 = (-$b - sqrt($delta)) / (2 * $a);
+echo "<br>O X1 é: $x1 e o X2 é: $x2<br>";
 
