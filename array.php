cd30e9b10427c6025b6064d95080ecb2b54d85f3
arielfelippi <41402358+arielfelippi@users.noreply.github.com>
2025-12-09 21:54:36 -0300

    Update array.php

--- array.php
+++ array.php
@@ -99,6 +99,16 @@ for ($i = 0; $i < 5 ; $i++) {
  * $arraySoma[15, 100, 80];
  */
 
+$arrayInicio = [5, 10, 50];
+$arrayFim = [10, 90, 30];
+$arraySoma = [];
+
+for($i = 0; $i < 3; $i++) {
+    $arraySoma[$i] = $arrayInicio[$i] + $arrayFim[$i];
+}
+
+echo "<br>Soma dos vetores:<br>";
+echo implode(", ", $arraySoma);
 
 /**
  * Multiplicar arrays:
@@ -106,5 +116,69 @@ for ($i = 0; $i < 5 ; $i++) {
  * $arrayFim = [10, 100, 3];
  * Resultado esperado:
  * $arrayMultiplicado[50, 1000, 24];
+ */
+
+$arrayInicio = [5, 10, 8];
+$arrayFim = [10, 100, 3];
+$arrayMultiplicado = [];
+
+for($i = 0; $i < 3; $i++) {
+    $arrayMultiplicado[$i] = $arrayInicio[$i] * $arrayFim[$i];
+}
+
+echo "<br>A multiplicação dos vetores:<br>";
+echo implode(", ", $arrayMultiplicado);
+
+/**
+ * Dada uma palavra informada pelo usuario,
+ * verificar se a mesma forma um palíndromo.
+ * 
+ * Ex.: ana, subi no onibus, kaiak, natan.
+ * 
+ * Dica: Um for dentro do outro.
+ */
+
+echo "<br>Palíndromo:<br>";
+
+$palavra = "subinoonibus";
+$tamanho = strlen($palavra) - 1;
+$palavraInvertida = "";
+
+for($i = $tamanho; $i >= 0; $i--) {
+    $palavraInvertida .= $palavra[$i];
+}
+
+/**
+ * 0 == "0" true
+ * 0 === "0" false
+ */
+$ehPalindromo = $palavra === $palavraInvertida; 
+
+if ($ehPalindromo) {
+    echo "<br>A palavra $palavra é palindromo!<br>";
+} else {
+    echo "<br>A palavra $palavra NÃO é palindromo!<br>";
+}
+
+
+/**
+ * Contar quantas vogais existem em um frase ou palavra.
+ * 
+ * $vogais = ['a', 'e', 'i', 'o', 'u'];
+ * $vogalMinuscula = strtolower($palavra[$i]);
+ * 
+ * $existeVogal = in_array($vogalMinuscula, $vogais);
+ * 
+ * Ex.: 
+ * $palavra = "infoserv";
+ * $palavra = "infosErv";
+ * 
+ * Saida esperada: 3 vogais.
+ */
+
+
+/**
+ * Contar quantas vogais existem em um frase ou palavra.
+ * Totalizando a quantidade de cada uma, ou seja, quantos A, quantos E...
  */
 
